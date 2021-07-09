fe35c856199d62cfaa530b6e6ba7cf7ab458e52d
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-07-09 19:47:02 +0530

    Create group_translate.py

---
+++ plugin/group_translate.py
@@ -0,0 +1,17 @@
+from pyrogram import Client, filters
+
+@app.on_message(filters.group & filters.command(["tr"]))
+async def left(client,message):
+	if (message.reply_to_message):
+		try:
+			lgcd = message.text.split("/tr")
+			lg_cd = lgcd[1].lower().replace(" ", "")
+			tr_text = message.reply_to_message.text
+			translator = Translator()
+			translation = translator.translate(tr_text,dest = lg_cd)
+			await message.reply_text(translation.text)
+		except :
+			await message.delete()
+	else:
+			 await message.delete()
+			 
