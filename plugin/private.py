ff31ab3d8585e8d1c75562ad0420c28a901263b9
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-07-09 19:21:29 +0530

    Update private.py

--- plugin/private.py
+++ plugin/private.py
@@ -1,1 +1,14 @@
+from pyrogram import Client, filters
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup
+)
 
+@Client.on_message(filters.private & filters.command(['start']))
+async def start(client, message):
+          await message.reply_text(
+          text =f"Hello **{message.from_user.first_name }** \n\n __I am simple Google Translater Bot \n I can translate any language to you selected language__",
+          reply_to_message_id = message.message_id,
+          reply_markup=InlineKeyboardMarkup(
+            [ [ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ],[InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/LNtechnical") ]   ]  ) )
+            
