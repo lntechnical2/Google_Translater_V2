0c2a168230d839659a361eb5b2f3be5f8b4ecaaf
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-07-10 17:28:14 +0530

    Update translate.py

--- plugins/translate.py
+++ plugins/translate.py
@@ -4,9 +4,8 @@ from pyrogram.types import (
     InlineKeyboardMarkup
 )
 from helper.database import find , insert 
-from helper.keybord(
-Keybord1, keybord2, keybord3, keybord4, keybord5, keybord6
-)
+from helper.keybord import Keybord1, keybord2, keybord3, keybord4, keybord5, keybord6
+
 
 @Client.on_message(filters.private & filters.command(['start']))
 async def start(client, message):
