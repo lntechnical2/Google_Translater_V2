17d09f54a264ab31008442878767cf65e1f275b4
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-07-10 17:12:47 +0530

    Update set&unset.py

--- plugins/set&unset.py
+++ plugins/set&unset.py
@@ -3,11 +3,12 @@ from pyrogram.types import (
     InlineKeyboardButton,
     InlineKeyboardMarkup
 )
-from helper.database import as data 
+from helper.database import set,unset 
+from helper.list import list
 
 @Clien.on_message(filters.private &filters.command(['unset']))
 async def unset(client,message):
-	data.unset(message.chat.id)
+	unset(message.chat.id)
 	await message.reply_text("Successfully removed custom default language")
 
 @Client.on_message(filters.private &filters.command(['set']))
@@ -22,7 +23,7 @@ async def set(client,message):
     	    			lgcd = list[cd]
     	    		except:
     	    			await message.reply_text("❗️ This language Not available in My List \n Or Check Your spelling 😉",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Check List 📑" ,url="https://raw.githubusercontent.com/lntechnical2/Google-Translater-/main/List/list.txt")]]))
-    	    		data.set(user_id,lgcd)
+    	    		set(user_id,lgcd)
     	    		await message.reply_text(f"Successfully set custom default language **{cd}**")
     	    else:
     	    		await message.reply_text(" Please use this command with an argument. \n **For Example:/set English**",reply_markup=InlineKeyboardMarkup([[	InlineKeyboardButton("How To Use",url = "https://youtu.be/dUYvenXiYKE")]]))
