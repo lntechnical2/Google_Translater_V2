24caac07ce3a0db07742a8b3b63b7bdaad91b4b8
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-07-10 13:12:52 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,9 +1,10 @@
 import pymongo 
 import os
 
+DB_NAME = os.environ.get("DB_NAME")
 DB_URL = os.environ.get("DB_URL","")
 mongo = pymongo.MongoClient(DB_URL)
-db = mongo["User"]
-dbcol = db["Data"]
+db = mongo[DB_NAME]
+dbcol = db["USER"]
 
 class Database:
