25cdb03f189a74fe17117ab1e8da34ef30f87bb9
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-07-10 18:15:38 +0530

    Update set&unset.py

--- plugins/set&unset.py
+++ plugins/set&unset.py
@@ -8,7 +8,7 @@ from helper.list import list
 
 @Client.on_message(filters.private &filters.command(['unset']))
 async def unset(client,message):
-	unset(message.chat.id)
+	await unset(message.chat.id)
 	await message.reply_text("Successfully removed custom default language")
 
 @Client.on_message(filters.private &filters.command(['set']))
@@ -23,7 +23,7 @@ async def set(client,message):
     	    			lgcd = list[cd]
     	    		except:
     	    			await message.reply_text("❗️ This language Not available in My List \n Or Check Your spelling 😉",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Check List 📑" ,url="https://raw.githubusercontent.com/lntechnical2/Google-Translater-/main/List/list.txt")]]))
-    	    		set(user_id,lgcd)
+    	    		await set(user_id,lgcd)
     	    		await message.reply_text(f"Successfully set custom default language **{cd}**")
     	    else:
     	    		await message.reply_text(" Please use this command with an argument. \n **For Example:/set English**",reply_markup=InlineKeyboardMarkup([[	InlineKeyboardButton("How To Use",url = "https://youtu.be/dUYvenXiYKE")]]))
