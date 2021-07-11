06b9f4bac773e10fd97d79e0277cc18007b2d486
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-07-11 08:49:23 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -16,10 +16,8 @@ def insert(chat_id):
             	pass
 
 def set(chat_id,lg_code):
-	 try:
-	 	dbcol.update_one({"_id":chat_id},{"$set":{"lg_code":lg_code}})
-	 except:
-	 	dbcol.insert_one({"_id":chat_id,"lg_code":lg_code})
+	 dbcol.update_one({"_id":chat_id},{"$set":{"lg_code":lg_code}})
+
 	 	
 	 	
 	 	
