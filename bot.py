1499d59430344a2bf91c856417eb8ec4001ac312
lokaman chendekar <82169706+lntechnical2@users.noreply.github.com>
2021-08-24 06:36:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import os
 
 TOKEN = os.environ.get("TOKEN", "")
 
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = int(os.environ.get("API_ID",12345))
 
 API_HASH = os.environ.get("API_HASH", "")
 
