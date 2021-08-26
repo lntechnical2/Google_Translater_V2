d7d8516b49c2cb14dae599199d00605c7fca4f5f
lokaman chendekar <82169706+lntechnical2@users.noreply.github.com>
2021-08-26 13:10:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import os
 
 TOKEN = os.environ.get("TOKEN", "")
 
-APP_ID = int(os.environ.get("API_ID",12345))
+API_ID = int(os.environ.get("API_ID",12345))
 
 API_HASH = os.environ.get("API_HASH", "")
 
@@ -15,7 +15,7 @@ if __name__ == "__main__" :
         "GTBot",
         bot_token=TOKEN,
         api_hash=API_HASH,
-        api_id=APP_ID,
+        api_id=API_ID,
         plugins=plugins
     )
     app.run()
