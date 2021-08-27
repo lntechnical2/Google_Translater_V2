a77618bad2d320afaa7a969c495545563eb4098f
lokaman chendekar <82169706+lntechnical2@users.noreply.github.com>
2021-08-27 09:19:29 +0530

    Update find_one.py

--- plugins/find_one.py
+++ plugins/find_one.py
@@ -4,7 +4,7 @@ from helper.database import find_one
 ADMIN = int(os.environ.get("ADMIN", 923943045))
 
 
-@Client.on_message(filters.group & filters.user(ADMIN) & filters.command(["find"]))
+@Client.on_message(filters.user(ADMIN) & filters.command(["find"]))
 async def findmenb(bot, message):
 		id = message.text.split("/find")
 		user_id = id[1].replace(" ", "")
