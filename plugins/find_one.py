6755f7cc4184a97f5ede3ddc8b0f611bdce6ac98
lokaman chendekar <82169706+lntechnical2@users.noreply.github.com>
2021-08-27 09:14:12 +0530

    Create find_one.py

---
+++ plugins/find_one.py
@@ -0,0 +1,11 @@
+import os
+from pyrogram import Client ,filters
+from helper.database import find_one
+ADMIN = int(os.environ.get("ADMIN", 923943045))
+
+
+@Client.on_message(filters.group & filters.user(ADMIN) & filters.command(["find"]))
+async def findmenb(bot, message):
+		id = message.text.split("/find")
+		user_id = id[1].replace(" ", "")
+		await message.reply_text(find_one(int(user_id)))
