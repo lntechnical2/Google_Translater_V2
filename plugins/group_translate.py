da080f9b9351469beb21e91135bedc195fb0f784
lokaman chendekar <82169706+lntechnical2@users.noreply.github.com>
2021-08-27 07:36:58 +0530

    Update group_translate.py

--- plugins/group_translate.py
+++ plugins/group_translate.py
@@ -1,9 +1,9 @@
-from pyrogram import Client, filters
 from googletrans import Translator
-
-
+from pyrogram import Client, filters
+from helper.list import list
+from helper.database import find_one
 @Client.on_message(filters.group & filters.command(["tr"]))
-async def grouptrans(client,message):
+async def left(client,message):
 	if (message.reply_to_message):
 		try:
 			lgcd = message.text.split("/tr")
@@ -11,9 +11,20 @@ async def grouptrans(client,message):
 			tr_text = message.reply_to_message.text
 			translator = Translator()
 			translation = translator.translate(tr_text,dest = lg_cd)
-			await message.reply_text(translation.text)
+			try:
+				for i in list:
+				if list[i]==translation.src:
+					fromt = i
+				if list[i] == translation.dest:
+					to = i 
+				await message.reply_text(f"Translated from **{fromt.capitalize()}** To **{to.capitalize()}**\n\n```{translation.text}```")
+			except:
+				await message.reply_text(f"Translated from **{translation.src}** To **{translation.dest}**\n\n```{translation.text}```")
+      			
+				
+			
 		except :
 			print("error")
 	else:
-			 print("error")
-			 
+			 ms = await message.reply_text("You can Use This Command by using reply to message")
+			 await ms.delete()
