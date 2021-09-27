b69ed7c4381c22ef4276c974aed27c999fd207a8
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-09-27 10:57:47 +0530

    Added Text to Speach

--- plugins/textospech.py
+++ plugins/textospech.py
@@ -21,9 +21,9 @@ def convert(text):
 @Client.on_message(filters.command("tts"))
 async def text_to_speech(_, message: Message):
     if not message.reply_to_message:
-        return await message.reply_text("💡 reply to some text...")
+        return await message.reply_text("💡 Reply to some text...")
     if not message.reply_to_message.text:
-        return await message.reply_text("💡 reply to some text...")
+        return await message.reply_text("💡 Reply to some text...")
     m = await message.reply_text("🔁 Processing...")
     text = message.reply_to_message.text
     try:
